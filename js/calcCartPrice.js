613bef6fc1697dc4b7720fc3ea5a4f0f115fb848
baukakzs <isenbekb7@gmail.com>
2024-03-24 03:27:36 +0500

    correct_stupid_mistakes

--- js/calcCartPrice.js
+++ js/calcCartPrice.js
@@ -1,14 +1,31 @@
 function calcCartPrice(){
-    const cartItems = document.querySelectorAll ('.cart-item')
 
-    let totalPrice = 0
+    const cartWrapper = document.querySelector('.cart-wrapper')
+    const priceElements = cartWrapper.querySelectorAll('.price__currency')
 
-    cartItems.forEach(function(item){
-        const amountEl = item.querySelector('[data-counter]')
-        const priceEl = item.querySelector('.price__currency')
-        const currentPrice = parseInt(amountEl.innerText) * parseInt(priceEl.innerText)
-        totalPrice += currentPrice
+    const totalPriceEl = document.querySelector('.total-price')
+
+    let priceTotal = 0 
+
+    priceElements.forEach(function(item){
+        const amountEl = item.closest('.cart-item').querySelector('[data-counter]')
+        
+
+        priceTotal += parseInt(item.innerText) * parseInt(amountEl.innerText)
     })
 
-    console.log(totalPrice);
+    totalPriceEl.innerText = priceTotal
+
+    // const cartItems = document.querySelectorAll ('.cart-item')
+
+    // let totalPrice = 0
+
+    // cartItems.forEach(function(item){
+    //     const amountEl = item.querySelector('[data-counter]')
+    //     const priceEl = item.querySelector('.price__currency')
+    //     const currentPrice = parseInt(amountEl.innerText) * parseInt(priceEl.innerText)
+    //     totalPrice += currentPrice
+    // })
+
+    // console.log(totalPrice);
 }

38486ce80ec3b4f534478118487574e7e8174c72
baukakzs <isenbekb7@gmail.com>
2024-03-24 01:12:43 +0500

    insert_data_into_product_in_the_cart

--- js/cart 01.js
+++ js/cart 01.js
@@ -18,5 +18,37 @@ window.addEventListener('click', function (event) {
 
 
         console.log(productInfo);
+
+        // Собранные данные подставим в шаблон для товара в корзине
+        const cartItemHTML = `<div class="cart-item" data-id="02">
+        <div class="cart-item__top">
+            <div class="cart-item__img">
+                <img src="img/roll/california-tempura.jpg" alt="">
+            </div>
+            <div class="cart-item__desc">
+                <div class="cart-item__title">${productInfo.title}</div>
+                <div class="cart-item__weight">6 шт. / 205г.</div>
+
+                <!-- cart-item__details -->
+                <div class="cart-item__details">
+
+                    <div class="items items--small counter-wrapper">
+                        <div class="items__control" data-action="minus">-</div>
+                        <div class="items__current" data-counter="">1</div>
+                        <div class="items__control" data-action="plus">+</div>
+                    </div>
+
+                    <div class="price">
+                        <div class="price__currency">1230 KZT</div>
+                    </div>
+
+                </div>
+                <!-- // cart-item__details -->
+
+            </div>
+        </div>
+    </div>`
+
+    
     }
 })

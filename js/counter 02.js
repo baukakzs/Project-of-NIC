d38475eca3f0fbeffd7d6e2b4cadf86d0026caac
baukakzs <isenbekb7@gmail.com>
2024-03-24 00:06:44 +0500

    check_button_m/p

--- js/counter 02.js
+++ js/counter 02.js
@@ -1,1 +1,13 @@
-// 
+// Добавляем прослушку на всем окне
+ window.addEventListener('click', function(event){
+
+    if(event.target.dataset.action === 'plus'){
+        console.log('Plus')
+    }
+
+    if(event.target.dataset.action === 'minus'){
+        console.log('Minus')
+    }
+
+
+ })

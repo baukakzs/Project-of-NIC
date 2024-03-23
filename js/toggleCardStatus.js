141f8ce85d67d4a3bdeaed3ef9a5775a0916138f
baukakzs <isenbekb7@gmail.com>
2024-03-24 02:37:34 +0500

    add_none_in_cart_is_empty

--- js/toggleCardStatus.js
+++ js/toggleCardStatus.js
@@ -2,12 +2,17 @@ function toggleCardStatus(){
 
     const cartWrapper = document.querySelector('.cart-wrapper')
     const cartEmptyBadge = document.querySelector('[data-cart-empty]')
+
+    const orderForm = document.querySelector('#order-form')
+
     if(cartWrapper.children.length > 0){
         console.log('FULL');
         cartEmptyBadge.classList.add('none')
+        orderForm.classList.remove('none')
     } else {
         console.log('EMPTY');
         cartEmptyBadge.classList.remove('none')
+        orderForm.classList.add('none')
     }
 
 }

90b659813c497df6a57df80e54a1c07376ee8cb8
baukakzs <isenbekb7@gmail.com>
2024-03-24 00:23:35 +0500

    find_counter_funcs

--- js/counter 02.js
+++ js/counter 02.js
@@ -1,13 +1,24 @@
 // Добавляем прослушку на всем окне
- window.addEventListener('click', function(event){
+window.addEventListener('click', function (event) {
+    // Находим обертку счетчика
+    const counterWrapper = event.target.closest('.counter-wrapper')
 
-    if(event.target.dataset.action === 'plus'){
-        console.log('Plus')
+    // Находим див с числом счетчика
+    const counter = counterWrapper.querySelector('[data-counter]')
+
+
+
+    if (event.target.dataset.action === 'plus') {
+
+        counter.innerText = ++counter.innerText
     }
 
-    if(event.target.dataset.action === 'minus'){
-        console.log('Minus')
+    if (event.target.dataset.action === 'minus') {
+
+        // Проверка счетчика больше
+        if (parseInt(counter.innerText) > 1) {
+            // Изменяем текст в счетчике уменьшая на 1 
+            counter.innerText = --counter.innerText
+        }
     }
-
-
- })
+})
